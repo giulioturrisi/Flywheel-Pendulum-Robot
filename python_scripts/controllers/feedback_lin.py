d19377f5a56dbf851605fe327f95a676107a9037
gturrisi <giulioturrisi@hotmail.it>
2022-12-22 18:00:18 +0100

    adding integral action to feedback lin and other pip install to the docker file

--- python_scripts/controllers/feedback_lin.py
+++ python_scripts/controllers/feedback_lin.py
@@ -24,13 +24,13 @@ class Feedback_Lin:
         self.k_d = k_d
         self.k_p = k_p
 
-        self.integral_error = 0
+        self.error_i = 0
 
         self.robot = Robot_dynamics()
 
 
     def compute_control(self, state, state_des):
-        """Compute PID control inputs
+        """Compute Feedback Lin control inputs
 
         Args:
             state (np.array): actual robot state
@@ -40,23 +40,27 @@ class Feedback_Lin:
             (np.array): control inputs
 
         """
+        error_p = state_des[0] - state[0]
+        error_d = state_des[1] - state[1]
+        self.error_i += error_p
 
         tau = -(self.robot.d22/self.robot.det_D)*(-self.robot.m_bar*self.robot.g*np.sin(state[0]))/(self.robot.d12/self.robot.det_D)
         
-        tau += -(self.k_p*(state_des[0] - state[0]) + self.k_d*(state_des[1] - state[1]))/(self.robot.d12/self.robot.det_D) 
+        tau += -(self.k_p*(error_p) + self.k_d*(error_d) + self.k_i*(self.error_i))/(self.robot.d12/self.robot.det_D) 
 
         
         return tau
 
 
 if __name__=="__main__":
-    controller = Feedback_Lin(k_p = 20, k_d = 0.5, k_i = 0.1)
+    controller = Feedback_Lin(k_p = 30, k_d = 5, k_i = 0.001)
     state = np.zeros(controller.state_dim)
-    state[0] = .5 
+    state[0] = 1.8
+    state[1] = 1.5 
     state_des = np.zeros(controller.state_dim)
     
     dt = 0.001
-    for j in range(0,5000):
+    for j in range(0,500):
         print("################")
         tau = controller.compute_control(state, state_des=state_des)
         tau = tau 
@@ -65,6 +69,7 @@ if __name__=="__main__":
 
         x_dot = controller.robot.forward_dynamics(state, tau)
         qdd = x_dot[1:3]
+        print("qdd: ", qdd)
 
         state = euler_integration.euler_integration(state, qdd, dt).reshape(controller.state_dim,)
 
